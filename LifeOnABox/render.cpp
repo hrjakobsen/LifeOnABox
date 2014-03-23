80938c818e009f5326be403df2aef9999227ff9d
HrJakobsen <mathias@jbcoding.com>
2014-03-23 22:50:42 +0100

    Added Reshape

--- LifeOnABox/render.cpp
+++ LifeOnABox/render.cpp
@@ -12,3 +12,22 @@ void display() {
 
 	glutSwapBuffers();
 }
+
+void reshape(int W, int H){
+	// Prevent a divide by zero, when window is too short
+	// (you can't make a window of zero width).
+	if (H == 0)
+		H = 1;
+	float ratio = (float)W / H;
+	// Use the Projection Matrix
+	glMatrixMode(GL_PROJECTION);
+	// Reset Matrix
+	glLoadIdentity();
+	// Set the viewport to be the entire window
+	glViewport(0, 0, W, H);
+	// Set the correct perspective.
+	gluPerspective(45, ratio, 0.01, 100);
+	// Get Back to the Modelview
+	glMatrixMode(GL_MODELVIEW);
+	display();
+}
