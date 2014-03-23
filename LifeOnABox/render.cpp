6aca6cf930f501cea01d76d1f5378dcd2892b3f2
jbcoding <mads12bjoern@gmail.com>
2014-03-23 14:57:06 -0700

    Wire test

--- LifeOnABox/render.cpp
+++ LifeOnABox/render.cpp
@@ -4,11 +4,7 @@ void display() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
 
-	glBegin(GL_TRIANGLE_FAN);
-	glVertex3f(1, 1, 1);
-	glVertex3f(-1, 1, 0);
-	glVertex3f(-1, -1, -1);
-	glEnd();
+	glutWireSphere(1, 20, 20);
 
 	glutSwapBuffers();
 }
