fe80a4fe845a3131d3848325df5843767a6c9e6c
jbcoding <mads12bjoern@gmail.com>
2014-03-23 14:49:36 -0700

    Display

--- LifeOnABox/render.cpp
+++ LifeOnABox/render.cpp
@@ -1,1 +1,14 @@
 #include "render.h"
+
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+
+	glBegin(GL_TRIANGLE_FAN);
+	glVertex3f(1, 1, 1);
+	glVertex3f(-1, 1, 0);
+	glVertex3f(-1, -1, -1);
+	glEnd();
+
+	glutSwapBuffers();
+}
